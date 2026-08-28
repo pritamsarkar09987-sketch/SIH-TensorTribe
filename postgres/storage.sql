c69d70a3a95d52dcf75c453d9e62b6d68934b093
Prkarz <prithvirajkar07@gmail.com>
2026-08-28 09:34:06 +0530

    DATABASE UPDATED

--- postgres/storage.sql
+++ postgres/storage.sql
@@ -2,11 +2,28 @@
 -- DATABASE SCHEMA
 -- =========================================
 
+--States
+CREATE TYPE status_cam AS ENUM ('online', 'offline', 'maintenance');
+CREATE TYPE alert_status AS ENUM ('active', 'resolved', 'false_positive');
+CREATE TYPE user_role AS ENUM ('admin', 'operator');
+
+
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role user_role DEFAULT 'operator',         
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Create a new table 'cameras' with a primary key and columns
 CREATE TABLE cameras (
     camera_id VARCHAR(50) PRIMARY KEY,
-    location_name TEXT NOT NULL CHECK (LENGTH(location_name) <= 100),
-rtsp_url TEXT NOT NULL CHECK (LENGTH(rtsp_url) <= 500),
+    camera_name TEXT NOT NULL CHECK (LENGTH(camera_name) <= 100),
+    location TEXT NOT NULL CHECK (LENGTH(location_name) <= 100),
+    virtual_fence JSONB NOT NULL,--JSONB data type to store the virtual fence coordinates in a flexible format
+    rtsp_url TEXT NOT NULL CHECK (LENGTH(rtsp_url) <= 500),
     status_cam VARCHAR(20) DEFAULT 'online'
 );
 
@@ -23,9 +40,8 @@ CREATE TABLE IF NOT EXISTS intrusion_alerts (
     alert_status VARCHAR(20) DEFAULT'active' --active, resolved, false_positive
 );
 
--- Index to speed up sorting and filtering by date/time
-CREATE INDEX idx_alerts_timestamp ON intrusion_alerts(event_timestamp);
+-- Index to speed up sorting and filtering by date/time wrt camera_id
+CREATE INDEX idx_alerts_timestamp ON intrusion_alerts(event_timestamp,camera_id);
 
--- Index to speed up finding alerts for a specific camera
-CREATE INDEX idx_alerts_camera ON intrusion_alerts(camera_id);
+
 
